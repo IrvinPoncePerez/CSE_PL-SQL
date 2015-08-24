027f361957350dc7af70c0bd6d03591d0a6bb84d
Irvin Ponce Perez <IrvinPoncePerez@gmail.com>
2015-08-24 17:29:42 -0500

    24-AGO-2015
    Pantalla de Visitas.

--- 20150810 - Pantalla de Visitas/4. Objetos SQL/PAC_VISITS_CONTROL_EXT_PKG.sql
+++ 20150810 - Pantalla de Visitas/4. Objetos SQL/PAC_VISITS_CONTROL_EXT_PKG.sql
@@ -1,9 +1,23 @@
 CREATE OR REPLACE PACKAGE PAC_VISITS_CONTROL_EXT_PKG IS
 
-    FUNCTION   IS_CHECK_EXISTS(FOLIO    VARCHAR2,   DATETIME    VARCHAR2)   RETURN BOOLEAN;
+    FUNCTION   IS_CHECK_EXISTS(
+                    FOLIO       VARCHAR2,   
+                    DATETIME    VARCHAR2)   RETURN BOOLEAN;
     
-    FUNCTION   IS_CREATE_CHECK(FOLIO    VARCHAR2,   DATETIME    VARCHAR2)   RETURN BOOLEAN;
+    FUNCTION   IS_CREATE_CHECK(
+                    FOLIO       VARCHAR2,   
+                    DATETIME    VARCHAR2)   RETURN BOOLEAN;
     
-    FUNCTION   GET_VISITOR_LENGTH_STAY(FOLIO    VARCHAR2)   RETURN VARCHAR2;
-
+    FUNCTION   GET_VISITOR_LENGTH_STAY(
+                    FOLIO       VARCHAR2)   RETURN VARCHAR2;
+    
+    PROCEDURE  PRINT_LABEL(
+                    ERRBUF                  OUT VARCHAR2,       
+                    RETCODE                 OUT VARCHAR2,
+                    P_FOLIO                 VARCHAR2,
+                    P_VISITS_NAME           VARCHAR2,
+                    P_VISITS_COMPANY        VARCHAR2,
+                    P_ASSOCIATE_PERSON      VARCHAR2,
+                    P_ASSOCIATE_DEPARTMENT  VARCHAR2);    
+                    
 END PAC_VISITS_CONTROL_EXT_PKG;
