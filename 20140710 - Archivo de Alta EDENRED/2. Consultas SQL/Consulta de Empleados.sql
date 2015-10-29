1538742784edbd076f18c62cd8620cd8f63096c2
Irvin Ponce Perez <IrvinPoncePerez@gmail.com>
2015-10-29 12:34:20 -0600

    29-OCT-2015
    Archivo de Alta EDENRED

--- 20140710 - Archivo de Alta EDENRED/2. Consultas SQL/Consulta de Empleados.sql
+++ 20140710 - Archivo de Alta EDENRED/2. Consultas SQL/Consulta de Empleados.sql
@@ -1,12 +1,12 @@
 SELECT 
-    PPPM.ATTRIBUTE1                                                          AS  NUM_CUENTA,
-    PAP.EMPLOYEE_NUMBER                                                      AS  NUM_EMPLEADO,
-    (PAP.FIRST_NAME || ' ' || PAP.MIDDLE_NAMES)                              AS  NOMBRES,
-    PAP.LAST_NAME                                                            AS  AP_PATERNO,
-    PAP.PER_INFORMATION1                                                     AS  AP_MATERNO,
-    REPLACE(PAP.PER_INFORMATION2, '-' , '')                                  AS  RFC,
-    PAP.NATIONAL_IDENTIFIER                                                  AS  CURP,
-    TO_CHAR(REPLACE(NVL(PAP.PER_INFORMATION3, '0'), '-', ''), '00000000000') AS  NUM_SEGURO
+    PPPM.ATTRIBUTE1                                                                AS  NUM_CUENTA,
+    PAP.EMPLOYEE_NUMBER                                                            AS  NUM_EMPLEADO,
+    (PAP.FIRST_NAME || ' ' || PAP.MIDDLE_NAMES)                                    AS  NOMBRES,
+    PAP.LAST_NAME                                                                  AS  AP_PATERNO,
+    PAP.PER_INFORMATION1                                                           AS  AP_MATERNO,
+    REPLACE(PAP.PER_INFORMATION2, '-' , '')                                        AS  RFC,
+    PAP.NATIONAL_IDENTIFIER                                                        AS  CURP,
+    TRIM(TO_CHAR(REPLACE(NVL(PAP.PER_INFORMATION3, '0'), '-', ''), '00000000000')) AS  NUM_SEGURO
   FROM PER_ALL_PEOPLE_F                     PAP,
        PER_ALL_ASSIGNMENTS_F                PAA,
        PAY_PAYROLLS_F                       PP,
@@ -17,16 +17,17 @@ SELECT
    AND PAP.PERSON_ID = PAA.PERSON_ID
    AND PAA.PAYROLL_ID = PP.PAYROLL_ID
    AND PAA.ASSIGNMENT_ID = PPPM.ASSIGNMENT_ID
-   AND SUBSTR(PP.PAYROLL_NAME, 1, 2) = :P_COMPANY_ID
+   AND SUBSTR(PP.PAYROLL_NAME, 1, 2) = P_COMPANY_ID
    AND PAP.PERSON_ID = PPTU.PERSON_ID
-   AND (PPTU.EFFECTIVE_START_DATE BETWEEN :var_start_date AND :var_end_date)
+   AND (PPTU.EFFECTIVE_START_DATE BETWEEN var_start_date AND var_end_date)
    AND PAP.PERSON_ID = PPS.PERSON_ID
-   AND PPPM.ORG_PAYMENT_METHOD_ID = :P_PAYMENT_METHOD_ID
+   AND PPPM.ORG_PAYMENT_METHOD_ID = P_PAYMENT_METHOD_ID
    AND (PAP.EFFECTIVE_START_DATE = PPTU.EFFECTIVE_START_DATE
     AND PAP.EFFECTIVE_START_DATE = PPS.DATE_START)
    AND PPS.ACTUAL_TERMINATION_DATE IS NULL
    AND PAA.PERIOD_OF_SERVICE_ID = PPS.PERIOD_OF_SERVICE_ID
    AND PPPM.OBJECT_VERSION_NUMBER = (SELECT MAX(PPPM1.OBJECT_VERSION_NUMBER)
                                        FROM PAY_PERSONAL_PAYMENT_METHODS_F  PPPM1
-                                      WHERE PPPM1.PERSONAL_PAYMENT_METHOD_ID = PPPM.PERSONAL_PAYMENT_METHOD_ID)
+                                      WHERE PPPM1.PERSONAL_PAYMENT_METHOD_ID = PPPM.PERSONAL_PAYMENT_METHOD_ID);
+
    
