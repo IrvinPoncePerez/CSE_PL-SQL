3129144e8d7b7c040ca99c1f7423257cc4805778
Irvin Ponce Perez <IrvinPoncePerez@gmail.com>
2016-01-23 12:51:03 -0600

    23-ENERO-2016
    Informe de Ausencias e Incidencias IMSS

--- 20140628 - Informe de Ausencias/4. Objetos SQL/PAC_GET_DISABILITIES_DAYS.sql
+++ 20140628 - Informe de Ausencias/4. Objetos SQL/PAC_GET_DISABILITIES_DAYS.sql
@@ -10,6 +10,9 @@ IS
     var_last_day           DATE;
     var_days               NUMBER := 0;
     var_month              NUMBER;
+    
+    var_min_year           NUMBER;
+    var_max_year           NUMBER;
 BEGIN
     
     var_min_month := TO_NUMBER(EXTRACT(MONTH FROM P_REGISTRATION_DATE));
@@ -19,39 +22,56 @@ BEGIN
     var_first_day := TRUNC(TO_DATE('01/' || var_month || '/' || P_YEAR, 'DD/MM/YYYY'), 'mm');
     var_last_day := TRUNC(LAST_DAY(TO_DATE('01/' || var_month || '/' || P_YEAR, 'DD/MM/YYYY')));
     
-    
-    
-    dbms_output.put_line(' var_min_month : ' || TO_CHAR(var_min_month) || 
-                         ' var_max month : ' || TO_CHAR(var_max_month) || 
-                         ' var_month : ' || TO_CHAR(var_month) || 
-                         ' var_first_day : ' || TO_CHAR(var_first_day) ||
-                         ' var_last_day : ' || TO_CHAR(var_last_day));
+    var_min_year := TO_NUMBER(EXTRACT (YEAR FROM P_REGISTRATION_DATE));
+    var_max_year := TO_NUMBER(EXTRACT (YEAR FROM P_REGISTRATION_EXP_DATE));
     
     
     
+    dbms_output.put_line(' var_min_month : '   || TO_CHAR(var_min_month) || 
+                            ' var_max month : '   || TO_CHAR(var_max_month) || 
+                            ' var_month : '       || TO_CHAR(var_month)     || 
+                            ' var_first_day : '   || TO_CHAR(var_first_day) ||
+                            ' var_last_day : '    || TO_CHAR(var_last_day)  ||
+                            ' var_min_year : '    || TO_CHAR(var_min_year)  ||
+                            ' var_max_year : '    || TO_CHAR(var_max_year));
+    
+    
+    
+    IF var_min_year = var_max_year THEN    
+        IF P_MONTH >  var_min_month AND P_MONTH < var_max_month THEN
         
-    IF P_MONTH >  var_min_month AND P_MONTH < var_max_month THEN
-    
-        var_days := (var_last_day - (var_first_day - 1));
-        dbms_output.PUT_LINE('Condición 1: ' || TO_CHAR(var_last_day) || ' - ' || TO_CHAR(var_first_day) || ' var_days : ' || TO_CHAR(var_days));
-    
-    ELSIF P_MONTH =  var_min_month AND P_MONTH = var_max_month THEN
+            var_days := (var_last_day - (var_first_day - 1));
+            dbms_output.PUT_LINE('Condición 1: ' || TO_CHAR(var_last_day) || ' - ' || TO_CHAR(var_first_day) || ' var_days : ' || TO_CHAR(var_days));
         
-        var_days := (P_REGISTRATION_EXP_DATE - (P_REGISTRATION_DATE - 1));
-        dbms_output.PUT_LINE('Condición 2: ' || TO_CHAR(P_REGISTRATION_EXP_DATE) || ' - ' || TO_CHAR(P_REGISTRATION_DATE) || ' var_days : ' || TO_CHAR(var_days));
+        ELSIF P_MONTH =  var_min_month AND P_MONTH = var_max_month THEN
+            
+            var_days := (P_REGISTRATION_EXP_DATE - (P_REGISTRATION_DATE - 1));
+            dbms_output.PUT_LINE('Condición 2: ' || TO_CHAR(P_REGISTRATION_EXP_DATE) || ' - ' || TO_CHAR(P_REGISTRATION_DATE) || ' var_days : ' || TO_CHAR(var_days));
+            
         
-    
-    ELSIF P_MONTH > var_min_month AND P_MONTH = var_max_month  THEN
-    
-        var_days := (P_REGISTRATION_EXP_DATE - (var_first_day - 1));
-        dbms_output.PUT_LINE('Condición 3: ' || TO_CHAR(P_REGISTRATION_EXP_DATE) || ' - ' || TO_CHAR(var_first_day) || ' var_days : ' || TO_CHAR(var_days));
+        ELSIF P_MONTH > var_min_month AND P_MONTH = var_max_month  THEN
         
-    ELSIF P_MONTH = var_min_month AND P_MONTH < var_max_month THEN
+            var_days := (P_REGISTRATION_EXP_DATE - (var_first_day - 1));
+            dbms_output.PUT_LINE('Condición 3: ' || TO_CHAR(P_REGISTRATION_EXP_DATE) || ' - ' || TO_CHAR(var_first_day) || ' var_days : ' || TO_CHAR(var_days));
+            
+        ELSIF P_MONTH = var_min_month AND P_MONTH < var_max_month THEN
+            
+            var_days := (var_last_day - (P_REGISTRATION_DATE - 1));
+            dbms_output.PUT_LINE('Condición 4: ' || TO_CHAR(var_last_day) || ' - ' || TO_CHAR(P_REGISTRATION_DATE) || ' var_days : ' || TO_CHAR(var_days));
+            
+        END IF;
+    ELSIF var_min_year < var_max_year THEN
+        IF var_month > var_max_month AND var_month <= var_min_month THEN
+            
+            var_days := (var_last_day - (P_REGISTRATION_DATE - 1));
+            dbms_output.PUT_LINE('Condición 5 Similar a Condición 4: ' || TO_CHAR(var_last_day) || ' - ' || TO_CHAR(P_REGISTRATION_DATE) || ' var_days : ' || TO_CHAR(var_days));
+            
+        ELSIF var_month >= var_max_month AND var_month < var_min_month THEN
         
-        var_days := (var_last_day - (P_REGISTRATION_DATE - 1));
-        dbms_output.PUT_LINE('Condición 4: ' || TO_CHAR(var_last_day) || ' - ' || TO_CHAR(P_REGISTRATION_DATE) || ' var_days : ' || TO_CHAR(var_days));
-        
-    
+            var_days := (P_REGISTRATION_EXP_DATE - (var_first_day - 1));
+            dbms_output.PUT_LINE('Condición 6 Similar a Condición 3: ' || TO_CHAR(P_REGISTRATION_EXP_DATE) || ' - ' || TO_CHAR(var_first_day) || ' var_days : ' || TO_CHAR(var_days));
+            
+        END IF;
     END IF;
     
     dbms_output.put_line(' ');
