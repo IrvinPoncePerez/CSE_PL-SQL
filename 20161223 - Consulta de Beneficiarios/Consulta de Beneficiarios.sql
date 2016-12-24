99ed631e825db89482f401be44b5a3c5492f68d5
Irvin Ponce Perez <IrvinPoncePerez@gmail.com>
2016-12-24 11:28:23 -0600

    2016 - Diciembre - 24 Beneficiarios
    Consulta de beneficiarios de empleados

--- 20161223 - Consulta de Beneficiarios/Consulta de Beneficiarios.sql
+++ 20161223 - Consulta de Beneficiarios/Consulta de Beneficiarios.sql
@@ -1,35 +1,26 @@
-
-SELECT *
-  FROM per_contact_relationships
- WHERE (person_id = 1720);
- 
-select *
-  from per_people_f
- where 1 = 1
-   and person_id = 33972;
-   
-   
-   
-
-
-
-
 SELECT DISTINCT
        EMP.EMPLOYEE_NUMBER      "Numero de Empleado",
        EMP.FULL_NAME            "Nombre de Empleado",
        EMP.USER_PERSON_TYPE     "Tipo Persona",
+       EMP.NAME                 "Departamento",
        CON.FULL_NAME            "Nombre de Contacto",
        PPT.USER_PERSON_TYPE     "Tipo Contacto"
   FROM (SELECT EMP.PERSON_ID,
                EMP.EMPLOYEE_NUMBER,
                EMP.FULL_NAME,
-               PPT.USER_PERSON_TYPE
-          FROM PER_PEOPLE_F     EMP,
-               PER_PERSON_TYPES PPT
+               PPT.USER_PERSON_TYPE,
+               HOU.NAME
+          FROM PER_PEOPLE_F             EMP,
+               PER_PERSON_TYPES         PPT,
+               PER_ASSIGNMENTS_F        PAF,
+               HR_ORGANIZATION_UNITS    HOU
          WHERE 1 = 1 
-           AND SYSDATE BETWEEN EMP.EFFECTIVE_START_DATE AND EMP.EFFECTIVE_END_DATE
+           AND PAF.PERSON_ID = EMP.PERSON_ID
            AND EMP.PERSON_TYPE_ID = PPT.PERSON_TYPE_ID
+           AND HOU.ORGANIZATION_ID = PAF.ORGANIZATION_ID
            AND PPT.SYSTEM_PERSON_TYPE = 'EMP'
+           AND SYSDATE BETWEEN EMP.EFFECTIVE_START_DATE AND EMP.EFFECTIVE_END_DATE
+           AND SYSDATE BETWEEN PAF.EFFECTIVE_START_DATE AND PAF.EFFECTIVE_END_DATE
         ) EMP
   LEFT JOIN PER_CONTACT_RELATIONSHIPS   PCR
          ON PCR.PERSON_ID = EMP.PERSON_ID
