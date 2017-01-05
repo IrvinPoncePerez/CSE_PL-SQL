4e8069bf914bf35a7c7fbe4aefeedcf40eb8c9c5
Irvin Ponce Perez <IrvinPoncePerez@gmail.com>
2017-01-05 17:58:21 -0600

    2017 - Enero - 05 RFC
    Actualización de RFC.

--- 20161223 - Actualización de RFC/PAC_RFC_SAT_PKG BODY.sql
+++ 20161223 - Actualización de RFC/PAC_RFC_SAT_PKG BODY.sql
@@ -69,6 +69,8 @@ CREATE OR REPLACE PACKAGE BODY PAC_RFC_SAT_PKG AS
         FND_FILE.PUT_LINE(FND_FILE.LOG, 'var_assign_payroll_warning : ' || (CASE WHEN var_assign_payroll_warning = TRUE THEN 'TRUE' ELSE 'FALSE' END));
         FND_FILE.PUT_LINE(FND_FILE.LOG, 'var_orig_hire_warning : ' || (CASE WHEN var_orig_hire_warning = TRUE THEN 'TRUE' ELSE 'FALSE' END));
     
+        P_RETCODE := 2;
+    
     EXCEPTION WHEN OTHERS THEN
         P_RETCODE := 1;
         P_ERRBUF := 'Error al actualizar el RFC. ' || SQLERRM;
