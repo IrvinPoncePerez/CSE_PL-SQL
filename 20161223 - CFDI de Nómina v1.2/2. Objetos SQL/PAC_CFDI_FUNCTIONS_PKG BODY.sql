2d26f20185f493be0511a39d648470fffe5a6456
Irvin Ponce Perez <IrvinPoncePerez@gmail.com>
2017-04-29 11:56:05 -0500

    2017.04.29 11.55
    CFDI de Nómina v1.2

--- 20161223 - CFDI de Nómina v1.2/2. Objetos SQL/PAC_CFDI_FUNCTIONS_PKG BODY.sql
+++ 20161223 - CFDI de Nómina v1.2/2. Objetos SQL/PAC_CFDI_FUNCTIONS_PKG BODY.sql
@@ -727,7 +727,7 @@ CREATE OR REPLACE PACKAGE BODY APPS.PAC_CFDI_FUNCTIONS_PKG AS
                                 PAC_CFDI_FUNCTIONS_PKG.GET_DIAPAG(PAA.ASSIGNMENT_ACTION_ID)
                             ELSE 1
                         END) <> 0
---                   AND PAPF.EMPLOYEE_NUMBER = 42
+                   AND PAPF.EMPLOYEE_NUMBER = 5483
                  GROUP BY PPF.PAYROLL_NAME,
                           FLV1.LOOKUP_CODE,
                           OI.ORG_INFORMATION2,
