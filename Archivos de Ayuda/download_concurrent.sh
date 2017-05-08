70b9061ddd0a58e549b9bad0a5cbb171dbd07f0d
Irvin Ponce Perez <IrvinPoncePerez@gmail.com>
2017-05-08 09:41:11 -0500

    2017.05.08 09.40
    Limpieza de archivos

--- Archivos de Ayuda/download_concurrent.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "$1"
-echo "$2"
-FNDLOAD apps/apps 0 Y DOWNLOAD $FND_TOP/patch/115/import/afcpprog.lct $2.ldt PROGRAM APPLICATION_SHORT_NAME="$1" CONCURRENT_PROGRAM_NAME="$2"
--- Archivos de Ayuda/upload_concurrent.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "$1"
-FNDLOAD apps/apps 0 Y UPLOAD $FND_TOP/patch/115/import/afcpprog.lct $1.ldt -WARNING=YES UPLOAD_MODE=REPLACE CUSTOM_MODE=FORCE
